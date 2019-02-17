134bf9764067f10d9921a140f1366b1a72e38aca
Manoj Kumar <manojkumaryyy@gmail.com>
2019-02-17 23:15:37 +0530

    Add selectfield component

--- app/components/FormComponents/Selectfield.jsx
+++ app/components/FormComponents/Selectfield.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
 
-const InputField = ({
+const SelectField = ({
   className,
   name,
   label,
   placeholder,
-  autocomplete,
   handleChange,
   isLableRequired,
   isRequired,
-  type,
-  error,
-  value
+  options,
+  value,
+  isMulti,
+  labelKey,
+  valueKey,
+  disabled,
+  simpleValue,
+  error
 }) => (
   <React.Fragment>
-    <div>
+    <div className="m-t-20">
       {isLableRequired &&
       <div className="input-field">
         <label htmlFor={label} className="field-label">{label}</label>
         {isRequired && (<span className="mandatory-marker">&nbsp;*</span>)}
       </div>
     }
-      <input
+      <Select
         name={name}
         className={className}
-        placeholder={placeholder}
         onChange={handleChange}
-        autoComplete={autocomplete}
-        type={type}
+        options={options}
+        placeholder={placeholder}
         value={value}
+        clearable={false}
+        multi={isMulti}
+        disabled={disabled}
+        simpleValue={!isMulti || simpleValue}
+        labelKey={labelKey}
+        valueKey={valueKey}
       />
       {
         error && <div className="error-message">{label} {error}</div>
@@ -38,30 +48,36 @@ const InputField = ({
   </React.Fragment>
 );
 
-InputField.propTypes = {
+SelectField.propTypes = {
   name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   className: PropTypes.string,
   isLableRequired: PropTypes.bool,
   placeholder: PropTypes.string,
   handleChange: PropTypes.func,
-  autocomplete: PropTypes.string,
   isRequired: PropTypes.bool,
-  type: PropTypes.string,
-  error: PropTypes.string,
-  value: PropTypes.string
+  labelKey: PropTypes.string.isRequired,
+  valueKey: PropTypes.string.isRequired,
+  isMulti: PropTypes.bool,
+  options: PropTypes.array,
+  disabled: PropTypes.bool,
+  simpleValue: PropTypes.bool,
+  value: PropTypes.string,
+  error: PropTypes.string
 };
 
-InputField.defaultProps = {
+SelectField.defaultProps = {
   isLableRequired: false,
   placeholder: '',
   className: '',
   handleChange: () => {},
-  autocomplete: 'on',
   isRequired: false,
-  type: 'text',
-  error: '',
-  value: ''
+  isMulti: false,
+  options: [],
+  disabled: false,
+  value: '',
+  simpleValue: true,
+  error: ''
 };
 
-export default InputField;
+export default SelectField;
