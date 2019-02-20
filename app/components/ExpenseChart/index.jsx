88d897cd9f92177b79a585b33e597a3e61a89348
Manoj Kumar <manojkumaryyy@gmail.com>
2019-02-21 01:07:00 +0530

    Integrate expense chart with api

--- app/components/ExpenseChart/index.jsx
+++ app/components/ExpenseChart/index.jsx
@@ -1,39 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import
-{ BarChart, Bar, LabelList, Label, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+{ BarChart, Bar, Label, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import '../../../assets/styles/components/Table.scss';
 import '../../../assets/styles/components/ExpenseTable.scss';
 
-const ExpenseChart = ({ expenses }) => (
-  <div className="table-container">
+const constructChartObject = expenses => {
+  const expenseChartData = [];
+  if (Object.keys(expenses).length > 0 && expenses[0] && expenses[0].$init) {
+    Object.keys(expenses).forEach(key => {
+      const { _doc } = expenses[key];
+      expenseChartData.push({ name: _doc.name, amount: expenses[key].amount });
+    });
+  }
+  return expenseChartData;
+};
+
+const ExpenseChart = ({ expenses }) => {
+  const expenseData = constructChartObject(expenses);
+  return (
     <div id="chart-container">
       <ResponsiveContainer>
         <BarChart
           width={700}
           height={400}
-          data={expenses}
+          data={expenseData}
           margin={{
             top: 5, right: 30, left: 20, bottom: 30
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
+          <CartesianGrid vertical horizontal={false} strokeDasharray="3 3" />
           <XAxis dataKey="name">
-            <Label htmlFor="Expense" value="Expense" position="left"> Expense </Label>
+            <Label htmlFor="Category" value="name" position="left"> Category </Label>
           </XAxis>
-          <YAxis label={{ value: 'Category', angle: -90, position: 'insideLeft' }} />
+          <YAxis label={{ value: 'Expense', angle: -90, position: 'insideLeft' }} />
           <Tooltip />
           <Legend />
-          <Bar dataKey="amount" fill="#ea751b" barSize={20} />
-          <LabelList dataKey="name" position="top" />
+          <Bar dataKey="amount" name="Amount" fill="#ea751b" barSize={20} />
         </BarChart>
       </ResponsiveContainer>
-    </div>
-  </div>
-);
+    </div>);
+};
 
 ExpenseChart.propTypes = {
-  expenses: PropTypes.array.isRequired
+  expenses: PropTypes.object.isRequired
 };
 
 export default ExpenseChart;
